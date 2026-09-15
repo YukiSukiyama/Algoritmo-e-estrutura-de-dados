72e86b43ab4e5a089437ae25864678562d250154
Murilo Yuki Sukiyama Pinto <muriloyukisukiyama21@dinfor.uniceub>
2026-09-14 21:46:14 -0300

    Continuacao aula 7

---
+++ Aula_7.2.py
@@ -0,0 +1,48 @@
+N = int(input())
+
+for _ in range(N):
+    texto = input()
+    resultado = []
+    i = 0
+
+    while i < len(texto):
+        if texto[i] == "0":
+            inicio = i
+
+            while i < len(texto) and texto[i] == "0":
+                i += 1
+            quantidade = i - inicio
+
+            if quantidade >= 3:
+                while quantidade > 255:
+                    resultado.append('#')
+                    resultado.append(chr(255))
+
+                    quantidade -= 255
+                resultado.append('#')
+                resultado.append(chr(quantidade))
+            else:
+                resultado.append('0' * quantidade )
+        #Referente aos espaços
+        elif texto[i] == "":
+            inicio = i
+
+            while i < len(texto) and texto[i] == "0":
+                i += 1
+            quantidade = i - inicio
+
+            if quantidade >= 3:
+                while quantidade > 255:
+                    resultado.append('$')
+                    resultado.append(chr(255))
+
+                    quantidade -= 255
+                resultado.append('$')
+                resultado.append(chr(quantidade))
+            else:
+                resultado.append('0' * quantidade )
+        else:
+            resultado.append(texto[i])
+            i += 1
+    print(''.join(resultado))
+            
